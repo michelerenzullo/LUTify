211eafbfb218b8bbf745c77f1b7b970c54ccd0cc
michelerenzullo <michele.renzullo98@gmail.com>
2021-03-02 22:32:05 +0100

    minor fix when output contains path

--- LUTify.py
+++ LUTify.py
@@ -62,7 +62,7 @@ if args.input.lower().endswith((".cube",".png",".jpg",".jpeg",".tiff")) and args
   title = re.search("[^\\\/]+(?=\.[\w]+$)",args.input)[0]
   if args.input.lower().endswith(".cube"):
    file = open(args.input,'r').read()
-   o_array = np.array([i.lower().replace(',', '').split() for i in re.findall("\n[+-]?[0-9]*[.]?[0-9]+\\s[+-]?[0-9]*[.]?[0-9]+\\s[+-]?[0-9]*[.]?[0-9]+",file)],dtype=float).reshape(-1)
+   o_array = np.array([i.lower().replace(',', '').split() for i in re.findall("\n[+-]?[0-9]*[.]?[0-9]+\s[+-]?[0-9]*[.]?[0-9]+\s[+-]?[0-9]*[.]?[0-9]+",file)],dtype=float).reshape(-1)
    lutSize = int(re.search("_SIZE.*?(\d+)",file).group(1))
    input_title = re.search("TITLE.?[\"'](.*?)[\"']",file)
    del file
@@ -112,7 +112,8 @@ if args.input.lower().endswith((".cube",".png",".jpg",".jpeg",".tiff")) and args
     
    if o_array.dtype == "float64":
     o_array = (o_array*255+.5).astype(np.uint8)
-   Image.fromarray(wrapper(standard,o_array,size)).save(standard + "_" + args.output, quality=100)
+   o_filename = re.search("[^\/]+$",args.output)[0]
+   Image.fromarray(wrapper(standard,o_array,size)).save(args.output[:-len(o_filename)] + standard + "_" + o_filename, quality=100)
    
  except FileNotFoundError:
-  print("Input error")
+  print("Please check input path or output path")
